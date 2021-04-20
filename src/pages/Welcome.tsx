0f92a038e357398994517b62ddfbc62d10d22515
Andre contiero <andre.contiero@yahoo.com.br>
2021-04-20 10:06:26 -0300

    Day 2, added pages and routing

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,35 +1,65 @@
-import React, { useState } from 'react'
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import { 
+  SafeAreaView, 
+  Text, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet,
+  Dimensions,
+  View
+} from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/core'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors'
-import { Button } from '../components/Button'
+import fonts from '../styles/fonts'
+
 
 export function Welcome () {
   
-  const [visible, setVisible] = useState(false);
+  const navigation = useNavigation();
 
-  function handleVisible () {
-    setVisible(true);
+  function handleStart() {
+    navigation.navigate('UserIdentification')
   }
 
   return (
     <SafeAreaView style={styles.container}>
 
-      <Text style={styles.title}>
-        Gerencie {'\n'} 
-        suas platas {'\n'} 
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
 
-      <Image source={wateringImg} style={styles.image}/>
+        <Text style={styles.title}>
+          Gerencie {'\n'} 
+          suas platas de {'\n'} 
+          forma fácil
+        </Text>
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regas suas plantas.
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+        <Image 
+        source={wateringImg} 
+        style={styles.image}
+        resizeMode='contain'
+        />
 
-      <Button title=">" />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regas suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+
+        <TouchableOpacity 
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+
+          <Feather 
+          name="chevron-right" 
+          style={styles.buttonIcon} 
+          />
+
+        </TouchableOpacity>
+
+      </View>
 
     </SafeAreaView>
   )
@@ -39,28 +69,50 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
+  },
+
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
 
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: "bold",
     textAlign: "center",
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
 
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
 
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
   },
+
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white
+  }
 
 })
---
+++ src/styles/fonts.ts
@@ -0,0 +1,7 @@
+export default {
+
+  heading: 'Jost_600SemiBold',
+  text: 'Jost_400Regular',
+  component: 'Jost_400Regular'
+
+};
